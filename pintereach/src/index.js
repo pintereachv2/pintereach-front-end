6842b0e1315a1c6745cbb1b363cfd41d56abc039
Hargobind Atwal <ethaipath@gmail.com>
2019-03-19 13:01:09 -0700

    a commit

--- pintereach/src/index.js
+++ pintereach/src/index.js
@@ -8,9 +8,8 @@ import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App';
 import reducer from './reducers'
-
-
 require('dotenv').config();
+
 
 const store = createStore(reducer, applyMiddleware(thunk));
 
